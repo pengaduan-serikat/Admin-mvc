90bd05a035e19263da1c8b87c688283121c9c172
ranggakusuma <ranggavskusuma@gmail.com>
2019-07-28 20:57:41 +0700

    add left join executor name for user

--- app/Http/Controllers/Api/Cases/DetailCaseController.php
+++ app/Http/Controllers/Api/Cases/DetailCaseController.php
@@ -23,9 +23,10 @@ class DetailCaseController extends Controller
   public function indexByUsers($id) {
     $case = Cases::where('cases.id', '=', $id)
             ->join('users as user', 'user.id', '=', 'cases.user_id')
+            ->leftJoin('users as executor', 'executor.id', '=', 'cases.executor_id')
             ->join('case_status', 'case_status.id', '=', 'cases.case_status_id')
             ->leftJoin('feedbacks', 'feedbacks.id', '=', 'cases.feedback_id')
-            ->select('cases.*', DB::raw("CONCAT(user.first_name, ' ', user.last_name) as user_full_name"), 'case_status.name as case_status', 'feedbacks.description as feedback')
+            ->select('cases.*', DB::raw("CONCAT(user.first_name, ' ', user.last_name) as user_full_name"), DB::raw("CONCAT(executor.first_name, ' ', executor.last_name) as executor_full_name"), 'case_status.name as case_status', 'feedbacks.description as feedback')
             ->first();
     return $case;
   }
